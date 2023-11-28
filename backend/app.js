3357e6ac343e5834fe0da0804ce2b0df25c33e26
stewie-gil <gilliganstewie7@gmail.com>
2023-11-28 16:10:23 +0300

    uri

--- backend/app.js
+++ backend/app.js
@@ -86,7 +86,7 @@ app.get('/', (req, res) => {
 
 
  
-const uri = "mongodb+srv://stewie-gil:777Stephen!@cluster0.ez5jfzu.mongodb.net/";
+const uri = "mongodb+srv://stewie-gil:###@cluster0.ez5jfzu.mongodb.net/";
 
 const connectDB = async () => {
   try {
